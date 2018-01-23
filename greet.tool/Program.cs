021100897fd015e96977ef09b41ab6efedc853a3
dasMulli <martin.andreas.ullrich@gmail.com>
2018-01-23 17:13:05 +0100

    Initial commit with tools examples

---
+++ greet.tool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace greet.tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hi {Environment.UserName}!");
+        }
+    }
+}
---
+++ sayhi.tool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sayhi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hi {Environment.UserName}!");
+        }
+    }
+}
